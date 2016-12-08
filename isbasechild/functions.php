1cb3acc7cde339858cc2da3a0b8f0bea0d95caa7
hiro-PC\hiro <info@inqsite.net>
2016-12-08 16:42:24 +0900

    親テーマのCSSの読み込み順が変わってしまうので、子テーマのfunction.phpを修正

--- isbasechild/functions.php
+++ isbasechild/functions.php
@@ -2,6 +2,8 @@
  
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
+    wp_enqueue_style( 'olein-layout-content-sidebar', get_template_directory_uri() . '/layouts/content-sidebar.css' );
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+wp_enqueue_style( 'isbase-style', get_stylesheet_uri());
  
 }
